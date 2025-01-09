7330030b4e2b337dd1d263b83923524ada889c47
vasu nageshri <vasu.nageshri@simformsolutions.com>
2025-01-09 11:44:32 +0530

    all about variable

---
+++ Syntax basics/variable.dart
@@ -0,0 +1,17 @@
+var fname = 'vasu'; 
+String lname = 'nageshri';
+
+// store null valur in variable
+
+String? name;
+
+// assert(false); //if condition give false then throw error
+
+late String address; //only declaration, we can initialize after in program
+
+final variable1; // final like constant but it all to not assign any value at declaration
+
+variable1 = 'value'; //only one time set value
+
+const variable2 = 'value'; //must assign at declaration
+
