62004fc6f023d30d211281efefe7475409846efc
vasu nageshri <vasu.nageshri@simformsolutions.com>
2025-01-09 19:33:22 +0530

    learn about control flow

---
+++ control flow/control_flow.dart
@@ -0,0 +1,68 @@
+//loops
+
+//for
+
+for(int i = 0; i<size;i++)
+{
+  //do something.....
+}
+
+//for-in
+for (var i in list/set){
+  //do something.....
+}
+
+// foreach
+list.forEach()
+
+//while
+while(condition){}
+
+//do-while
+do{
+
+}while();
+
+//branches
+
+//if-else
+
+if(condition){
+
+}else if(condition){
+
+}else{
+
+}
+
+//if-case
+if (pair case [int x, int y]) {
+  print('Was coordinate array $x,$y');
+} else {
+  throw FormatException('Invalid coordinates.');
+}
+
+//Switch
+switch(){
+ case a:
+    //do something...
+  default:
+    //do something...
+}
+
+//Error handling
+
+try{
+
+}catch(){
+// run this body if any error accure in try block
+}
+
+try{
+
+}catch(){
+
+}finally{
+// always execute 
+}
+ 
